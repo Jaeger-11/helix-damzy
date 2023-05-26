5ab2d092a0975ace91c6cc3b3a704054de065be7
Oluwadamilola Falodun <dpaulzs11@gmail.com>
2023-05-27 00:31:09 +0100

    javascript data push

--- app.js
+++ app.js
@@ -4,3 +4,46 @@ const pushTotalStaff = (total) => {
     document.querySelector('.totalstaff').innerHTML = total
 }
 pushTotalStaff(25)
+
+let jobData = {
+    owner: "john smith",
+    transport: '$1500',
+    title : "working as a cab driver",
+    requirements: [
+        { 
+            icon: 'images/government.svg',
+            requirement: "government work"
+        },
+        { 
+            icon: 'images/licence.svg',
+            requirement: "driving licence"
+        },
+        { 
+            icon: 'images/level.svg',
+            requirement: "2nd game level"
+        },
+        { 
+            icon: 'images/card.svg',
+            requirement: "salary on card"
+        },
+    ],
+    description: "Urban Ride is looking for experienced and professional taxi drivers who can deliver excellent service to our clients. You will have to drive our modern and comfortable vehicles, use our GPS and payment systems, and communicate effectively with our dispatchers and customers. You will also have to keep track of your mileage, fuel, and expenses."
+}
+
+const pushJobData = (job) => {
+    const { owner, title, requirements, description, transport } = job
+    document.querySelector('.ownername').innerHTML = owner;
+    document.querySelector('.description').innerHTML = description;
+    document.querySelector('.transportprice').innerHTML = transport;
+    document.querySelector('.title').innerHTML = title;
+    requirements.map((item) => {
+        const {icon, requirement} = item;
+        document.querySelector('.requirements-container').innerHTML += `
+        <div class="flexsmall requirement semibold fontmedium">
+            <img src=${icon} alt="icon" class="icon">
+            ${requirement}
+        </div>
+        ` 
+    } )
+}
+pushJobData(jobData);
